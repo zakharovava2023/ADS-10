89080a4c2d6c44c2e24fc2fbf214376886218d97
zakharovava2023 <125122557+zakharovava2023@users.noreply.github.com>
2023-05-22 19:01:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -20,7 +20,6 @@ std::vector<char> getPerm(const Tree& tree, int n) {
     std::vector<char> result;
     for (int i = 0; i < tree.getSize(); i++) {
         int temp = fact(ptr->getSize()-1);
-        int x = n / temp;
         ptr = ptr->getChild(x);
         result.push_back(ptr->getValue());
         n%=temp;
