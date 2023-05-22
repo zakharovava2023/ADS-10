8481efcdd27708e4eeeee6a13562357c84e2c418
zakharovava2023 <125122557+zakharovava2023@users.noreply.github.com>
2023-05-22 18:34:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,24 @@
 #include  <cstdlib>
 #include  "tree.h"
 
+uint64_t fact(uint16_t n) {
+    uint64_t result = 1;
+    for (uint64_t i = 1; i <= n; i++)
+        result *= i;
+    return result;
+}
+
 std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
+    if (tree.getSize() == 0 || n > fact(tree.getSize()))
+        return {};
+    n--;
+    const Tree* ptr = &tree;
+    std::vector<char> result;
+    for (int i = 0; i < tree.getSize(); i++) {
+        int temp = fact(ptr->getSize()-1);
+        ptr = ptr->getChild(n / temp);
+        result.push_back(ptr->getValue());
+        n%=temp;
+    }
+    return resul;
 }
